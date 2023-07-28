c6038a5bdf41cbe7b96347256ddc031f87fa29f0
Arif Kemal <arif162003@gmail.com>
2023-07-28 17:21:32 +0300

    first commit

--- Struct_Kavrami/Program.cs
+++ Struct_Kavrami/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Dikdortgen dikdortgen = new Dikdortgen();
+        dikdortgen.KisaKenar = 3;
+        dikdortgen.UzunKenar = 4;
+
+        System.Console.WriteLine("class alan hesabı {0}",dikdortgen.AlanHesapla()); 
+    }
+}
+
+class Dikdortgen{
+
+    public int KisaKenar;
+    public int UzunKenar;
+    public long AlanHesapla(){
+        return this.KisaKenar * this.UzunKenar;
+    }
+}
+
+
+struct Dikdortgen_Struct
+{
+    public int KisaKenar;
+    public int UzunKenar;
+    public long AlanHesapla(){
+        return this.KisaKenar * this.UzunKenar;
+    }
+}
